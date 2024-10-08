823bc36210c766ef3d08515b29c0b655e4ae8322
GarySegal <gary.segal@whitesourcesoftware.com>
2024-10-08 18:25:35 -0400

    initial commit

---
+++ cwe-78/std_thread.h
@@ -0,0 +1,29 @@
+/* The file is extracted from the NIST database https://samate.nist.gov/SARD/test-cases/248722/versions/2.0.0 */
+
+#ifndef _STD_THREAD_H
+#define _STD_THREAD_H
+
+typedef struct _stdThread *stdThread;
+typedef struct _stdThreadLock *stdThreadLock;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*stdThreadRoutine)(void *args);
+
+/* All of these return positive ('true') on success, zero ('false') on failure */
+int stdThreadCreate(stdThreadRoutine start, void *args, stdThread *thread);
+int stdThreadJoin(stdThread thread);
+int stdThreadDestroy(stdThread thread);
+
+int stdThreadLockCreate(stdThreadLock *lock);
+void stdThreadLockAcquire(stdThreadLock lock);
+void stdThreadLockRelease(stdThreadLock lock);
+void stdThreadLockDestroy(stdThreadLock lock);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
